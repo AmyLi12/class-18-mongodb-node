a2bd22a2cd2eeb1a38c7cc2d6147d3ff0752fc68
Amy Yi Li <142811190+AmyLi12@users.noreply.github.com>
2025-03-26 15:51:08 +0000

    completed class activity

--- server/server.mjs
+++ server/server.mjs
@@ -16,34 +16,33 @@ app.use((req, res, next) => {
 
 
 // Connect to MongoDB
-// import { MongoClient, ObjectId } from 'mongodb';
-// const client = new MongoClient('mongodb://localhost:27017');
-// const conn = await client.connect();
+import { MongoClient, ObjectId } from 'mongodb';
+const client = new MongoClient('mongodb://localhost:27017');
+const connection = await client.connect();
+const db = connection.db("app") // equivalent to mongosh's use command to select a database
 
-app.get('/api/documents.json', async (req, res) => {
-  res.json([
-    {
-      name: "Apple",
-      category: "fruit",
-      price: 1.99,
-      quantity: 150,
-      supplier: "Organic Farms Inc",
-      organic: true,
-      country: "USA",
-      expiration: new Date("2023-12-15")
-    },
-    {
-      name: "Banana",
-      category: "fruit",
-      price: 0.59,
-      quantity: 200,
-      supplier: "Tropical Imports",
-      organic: false,
-      country: "Ecuador",
-      expiration: new Date("2023-12-10")
-    }]).status(200);
+// Always have async in the code below
+app.get('/api/produce.json', async (req, res) => {
+  const produce = await db.collection('produce').find().toArray()
+  // toArray() will turn the data into a JSON. otherwise no data will show up
+
+  res.json(produce).status(200);
 });
 
+// Getting a single document
+// app.get('/api/produce/67e40f237e0b2cde5e6b140c.json', async (req, res) => {
+//   const banana = await db.collection('produce').findOne({
+//     _id: new ObjectId("67e40f237e0b2cde5e6b140c")}) // When working with object ids, need to convert the id string into an Object type
+//   res.json(banana).status(200)
+// });
+
+// Making the route work for any document
+app.get('/api/produce/:id.json', async (req, res) => {
+  const id = req.params.id
+  const banana = await db.collection('produce').findOne({
+    _id: new ObjectId(id)}) // When working with object ids, need to convert the id string into an Object type
+  res.json(banana).status(200)
+});
 
 // --- Change nothing below this line ---
 
